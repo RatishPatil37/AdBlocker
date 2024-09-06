06d2841ed07c4bb79f692eb767aac18bf65e8588
RatishPatil37 <160342661+RatishPatil37@users.noreply.github.com>
2024-09-06 07:39:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
-const wrapper = document.querySelector('.wrapper');
 
-signUpBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
-
-signInBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
