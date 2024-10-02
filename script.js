b93e600d5b4050d0ec6c2e1d9e527e6a23ef70d7
Abbas Shaikh <158834606+Abbas1608@users.noreply.github.com>
2024-10-02 22:21:35 +0530

    Update script.js
    Add download button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     document.getElementById('downloadButton').addEventListener('click', function() {
-        const extensionUrl = './assets/extension.zip'; // Corrected spelling of 'extension'
+        const extensionUrl = './extension.zip'; // Corrected spelling of 'extension'
         const anchor = document.createElement('a');
         anchor.href = extensionUrl;
         anchor.download = 'AD_Shield_Extension.zip';  // Name of downloaded file
