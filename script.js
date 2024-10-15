ff62596c40ba9215e47dee5916fb3e5c31337cf7
RatishPatil37 <160342661+RatishPatil37@users.noreply.github.com>
2024-10-15 19:55:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('downloadButton').addEventListener('click', () => {
     let zip = new JSZip();
-    let jsonFolder = zip.folder('jsonfiles');
+    let jsonFolder = zip.folder('AD_SHIELD');
     
     jsonFolder.file('manifest.json', `{
-        "manifest_version": 3, "name": "My Extension", "version": "1.0.0" 
+        "manifest_version": 3, "name": "AD_SHIELD", "version": "1.0.0" 
     }`);
     jsonFolder.file('rules.json', `{
         "rule1": "some rule", "rule2": "another rule"
@@ -12,7 +12,7 @@ document.getElementById('downloadButton').addEventListener('click', () => {
     zip.generateAsync({ type: "blob" }).then(content => {
         const link = document.createElement("a");
         link.href = URL.createObjectURL(content);
-        link.download = "jsonfiles.zip";
+        link.download = "AD_SHIELD.zip";
         link.click();
     });
 });
